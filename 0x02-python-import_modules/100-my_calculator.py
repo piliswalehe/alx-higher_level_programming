72172d97d648e557cde805dc0d1b471cde2082b6
piliswalehe <swalehe_pili@yahoo.com>
2023-11-30 13:58:30 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -21,4 +21,3 @@ if __name__ == "__main__":
             print("{} / {} = {}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)

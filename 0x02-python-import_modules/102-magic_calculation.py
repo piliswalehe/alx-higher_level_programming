69f038cd11aa928af056fe97840f52e1edd2334e
piliswalehe <swalehe_pili@yahoo.com>
2023-12-01 13:20:41 -0800

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import dis
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a > b:
@@ -6,4 +7,4 @@ def magic_calculation(a, b):
     else:
         result = (a - b) * 2
     return result
-dis.dis(magic_calculation)      
+dis.dis(magic_calculation)

0e3503953d320db7f087e5066898d35d989e7fee
piliswalehe <swalehe_pili@yahoo.com>
2024-01-04 12:19:46 -0800

    4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -33,7 +33,7 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
         if self.__width = 0 or self.__height == 0:

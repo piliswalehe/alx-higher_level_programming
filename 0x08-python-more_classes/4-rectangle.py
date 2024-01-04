2910e87f484e52314fa1db1929bf2ae70ecad952
piliswalehe <swalehe_pili@yahoo.com>
2024-01-04 12:07:42 -0800

    4-rectangle.py

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+
+
+class Rectangle:
+    """Rectangle class"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0::
+            return "Rectangle(" + str(self.width) + ", " + str(self.height) + ")"
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if not self.width or not self.height:
+            return 0
+        return 2 * (self.height + self.width)
+
+    def __str__(self):
+        if not self.__width == 0 or self.__height == 0:
+            return ""
+        return (("#" * self.__width + "\n") * self.__height)[:-1]
+
+    def __repr__(self):
+        return "Rectangle(" + str(self.width) + ", " + str(self.height) + ")"

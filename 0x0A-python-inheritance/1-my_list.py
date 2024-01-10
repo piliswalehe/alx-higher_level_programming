431d1a269d1c59f6c5391b71db50403f3d5f594a
piliswalehe <swalehe_pili@yahoo.com>
2024-01-10 04:36:42 -0800

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,7 +8,7 @@ class Mylist(list):
     """defines a new class called MyList that inherits from a list class
     """
     def print_sorted(self):
-        """print elements in a list in order
-        """
+        """print elements in a list in order"""
+        
         sorted_list = sorted(self)
         print(sorted_list)

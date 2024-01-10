fa31cd68fd5866355af7f4a660de33229de84320
piliswalehe <swalehe_pili@yahoo.com>
2024-01-10 06:06:17 -0800

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,9 +5,9 @@ Contains definiton for the class MyList that inherits from list.
 
 
 class Mylist(list):
-    """defines a new class called MyList that inherits from a list class
+    """defines a class MyList that inherits from a list class
     """
-
     def print_sorted(self):
         """print elements in a list in order"""
+
         print(sortedlist)
